c4a3991e6fc1805e9449f6acf8bff4596773ac91
Dtoce <54190061+Dtoce@users.noreply.github.com>
2022-05-12 22:35:02 +0300

    Uzdevumi

---
+++ d_uzd.py
@@ -0,0 +1,7 @@
+"""
+Izveidot apļveida diagrammu ar datiem par firmām Pankūkas, Biete, 
+Bumbieris, Kartupelis, kuru dalība
+projektā attiecīgi ir 35, 25, 25 un 15 mēneši.
+
+Diagrammā izcelt Pankūkas un Kartupeļus, kā piemēru skatīt d_uzd.png.
+"""
