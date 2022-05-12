342bd3c2e452064b90398dca751dfda2f6757989
Dtoce <54190061+Dtoce@users.noreply.github.com>
2022-05-12 22:32:47 +0300

    Sagataves
    Uzdevumu sagataves (uzdevumu formulējums), palīgfaili.

---
+++ e_uzd.py
@@ -0,0 +1,9 @@
+"""
+linspace (sākums, beigas, num, beigu punkts = True, retstep = False,
+ dtype = None , axis = 0 ) 
+linespace - atgriezt vienādi izvietotus skaitļus noteiktā intervālā.
+
+Izveidot sin funkcijas grafiku ar sākumu 0, beigās -200, bet skaitļu skaits 100.
+
+
+"""
---
+++ f_uzd.py
@@ -0,0 +1,6 @@
+"""
+Ierakstīt, noskrāpējot datus, texta failā f_uzd.txt visas profesijas, firmas,
+ kuras tās piedāvā, kā arī firmu adreses. 
+ Terminālī izdrukāt piedāvāto  profesiju skaitu.
+ url - https://realpython.github.io/fake-jobs/
+"""
