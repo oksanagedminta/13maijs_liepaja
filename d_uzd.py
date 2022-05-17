362d503458e1d8cb41f4a8812ad20a9d28b11d05
Oksana <oksana.tkaca@gmail.com>
2022-05-17 23:20:21 +0300

    d_uzd_ok

--- d_uzd.py
+++ d_uzd.py
@@ -5,3 +5,14 @@ projektā attiecīgi ir 35, 25, 25 un 15 mēneši.
 
 Diagrammā izcelt Pankūkas un Kartupeļus, kā piemēru skatīt d_uzd.png.
 """
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+y=np.array([35, 25, 25, 15])
+#print(y)   #pārbaudām
+mylabels=["Pankūkas", "Biete", "Bumbieris", "Kartupelis"]
+aka=[0, 0.2, 0, 0.2]    # lai izceļ no apļa
+
+plt.pie(y, labels=mylabels, shadow=True, explode=aka)
+plt.show()
