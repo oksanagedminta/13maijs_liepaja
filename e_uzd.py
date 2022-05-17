5c933cbe5e1142713f5fff4ab80ad9f95e047885
Oksana <oksana.tkaca@gmail.com>
2022-05-17 23:20:35 +0300

    e_uzd_ok

--- e_uzd.py
+++ e_uzd.py
@@ -5,5 +5,15 @@ linespace - atgriezt vienādi izvietotus skaitļus noteiktā intervālā.
 
 Izveidot sin funkcijas grafiku ar sākumu 0, beigās -200, bet skaitļu skaits 100.
 
-
+linspace ir iekš numpy
 """
+import matplotlib.pyplot as plt
+import numpy as np
+
+x=np.linspace(0, 50, 100)
+y=np.sin(x)
+
+plt.plot(x, y)
+
+plt.xticks(rotation="vertical") # x ass ciparus pagriežam
+plt.show()
